acf680d6a6212f2fc68d668456315aa99a64816b
Mohammed Anas <mohammedanas1571@gmail.com>
2024-02-28 20:56:43 +0530

    sign in back end

--- api/Router/authRouter.js
+++ api/Router/authRouter.js
@@ -1,9 +1,10 @@
 import express from  'express';
 const router = express.Router();    
 
-import { userSignup } from '../controller/authController.js';
+import { userSignup,userLogin } from '../controller/authController.js';
 
 router.post("/signup",userSignup)
+router.post("/login",userLogin)
 
 
 export default router 
--- api/controller/authController.js
+++ api/controller/authController.js
@@ -1,23 +1,54 @@
+import bcrypt from "bcrypt";
+import User from "../models/user_model.js";
+import jwt from "jsonwebtoken";
+import { log } from "console";
 
-import bcrypt from 'bcrypt'
-import User from '../models/user_model.js'
-
-
-
-export const userSignup = async (req, res,next) => {
+export const userSignup = async (req, res, next) => {
   const { username, email, password } = req.body;
   try {
-    if(username == ""|| email == ""|| password==''){res,status(500).json("all fields are required")}
+    if (username == "" || email == "" || password == "") {
+      res, status(500).json("all fields are required");
+    }
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({
       username,
       email,
-      password: hashedPassword
+      password: hashedPassword,
     });
     console.log(newUser);
-    await newUser.save(); 
+    await newUser.save();
     res.status(200).json({ message: "User registered" });
   } catch (error) {
-   next(error)
+    next(error);
+  }
+};
+
+export const userLogin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json("Invalid Credentials");
+    }
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+      return res.status(404).json("password is wrong");
+    }
+    console.log(process.env.TOKEN);
+    const token = jwt.sign({ id: user._id }, process.env.TOKEN, {
+      expiresIn: "7d",
+    });
+    const { password: hashedPassword, ...rest } = user._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    res.status(500).json(error.message);
   }
 };
